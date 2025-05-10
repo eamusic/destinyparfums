7cc544b7a77c666ea23ae3ea8acd64c84e775168
eamusic <edwandrade@gmail.com>
2025-05-10 15:36:25 -0500

    Se debe revisar los permisos para la carpeta views

--- src/routes/home.js
+++ src/routes/home.js
@@ -11,11 +11,18 @@ const app = require('express');
 router.use(app.json());
 router.use(app.urlencoded({ extended: true })); // Middleware para parsear el cuerpo de las solicitudes
 router.use(app.static(path.join(__dirname, 'public'))); // Middleware para servir archivos estáticos
+// milddware para servir archivos estáticos de la carpeta views
 router.use(app.static(path.join(__dirname, 'views'))); // Middleware para servir archivos estáticos
+// router.use(app.static(path.join(__dirname, 'views'))); // Middleware para servir archivos estáticos
 
 router.get('/', (req, res) => {
     const title = 'Destiny Perfumeria';
     res.render('index', { title });
+});
+
+router.get('/shop', (req, res) => {
+    const title = 'Destiny Perfumeria - Shop';
+    res.render('pages/shop', { title });
 });
 
 module.exports = router;
