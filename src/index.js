2a00b60be3c670fe0587f7ef76133c83dda7e360
eamusic <edwandrade@gmail.com>
2025-05-10 20:53:59 -0500

    Modificadas las rutas y el motor render para las vistas con ejs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
+const ejs = require('ejs');
+const session = require('express-session');
 // const usersRoutes = require('./routes/users');
 const homeRoutes = require('./routes/home');
 // const dashboardRoutes = require('./routes/dashboard');
@@ -11,18 +13,20 @@ const homeRoutes = require('./routes/home');
 app.set('case sensitive routing', true);
 app.set('appName', 'Destiny Perfumeria');
 app.set('port', 3000);
-app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use('/public',express.static(path.join(__dirname, 'public'))); // Serve static files from the public directory
+// Serve static files from the views directory
+app.use('/public',express.static(path.join(__dirname, '/public')));
 
 app.use(express.json()); // Parse JSON bodies (as sent by API clients)
+app.use(express.urlencoded({ extended: true })); // Middleware para parsear el cuerpo de las solicitudes
 app.use(morgan('dev')); // Log requests to the console
 
 app.use(homeRoutes);
-/*app.use(usersRoutes);
-app.use(loginRoutes);
-app.use(dashboardRoutes);*/
+/*app.use(usersRoutes);*/
+// app.use(loginRoutes);
+// app.use(dashboardRoutes);
 
 app.listen(app.get('port'), () => {
     console.log(`${app.get('appName')} is running on port ${app.get('port')}`);
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const { Router } = require('express');
 const path = require('path');
 const session = require('express-session');
@@ -7,15 +8,17 @@ const router = Router();
 
 const app = require('express');
 
-
 router.use(app.json());
 router.use(app.urlencoded({ extended: true })); // Middleware para parsear el cuerpo de las solicitudes
-router.use(app.static(path.join(__dirname, 'public'))); // Middleware para servir archivos estáticos
-router.use(app.static(path.join(__dirname, 'views'))); // Middleware para servir archivos estáticos
 
 router.get('/', (req, res) => {
     const title = 'Destiny Perfumeria';
-    res.render('index', { title });
+    res.render('pages/index', { title });
+});
+
+router.get('/shop', (req, res) => {
+    const title = 'Destiny Perfumeria - Shop';
+    res.render('pages/shop', { title });
 });
 
 module.exports = router;
