715ba849743179ef2d23fdc86bf1cd3c8ac0ad32
eamusic <edwandrade@gmail.com>
2025-05-10 21:04:53 -0500

    Optimización del archivo de rutas home.js

--- src/routes/home.js
+++ src/routes/home.js
@@ -10,13 +10,6 @@ const app = require('express');
 
 router.use(app.json());
 router.use(app.urlencoded({ extended: true })); // Middleware para parsear el cuerpo de las solicitudes
-<<<<<<< HEAD
-=======
-router.use(app.static(path.join(__dirname, 'public'))); // Middleware para servir archivos estáticos
-// milddware para servir archivos estáticos de la carpeta views
-router.use(app.static(path.join(__dirname, 'views'))); // Middleware para servir archivos estáticos
-// router.use(app.static(path.join(__dirname, 'views'))); // Middleware para servir archivos estáticos
->>>>>>> 7cc544b7a77c666ea23ae3ea8acd64c84e775168
 
 router.get('/', (req, res) => {
     const title = 'Destiny Perfumeria';
